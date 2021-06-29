ee49949097b123b3d2b1556b9f7a18a9592a9eb3
Ricieri72 <ricieri.silva@fatec.sp.gov.br>
2021-06-29 02:06:03 -0300

    criando arquivo index.html e javascript

---
+++ Javascript/js/main.js
@@ -0,0 +1,23 @@
+var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas[0].nome);
+console.log(frutas[1].cor);
+/* var fruta = { nome: "maça", cor: "vermelha" };
+console.log(fruta.nome);
+alert(fruta.cor);
+var nome = "Ricieri da Silva";
+var idade = "49";
+var idade2 = "10";
+var frase = "Japão é o melhor time do mundo";
+var lista = ["maça", "pera", "laranja", "laranja"];
+lista.pop();
+lista.push("uva");
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+// alert(nome + " tem " + idade + " anos !");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil")); */
